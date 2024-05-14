5714cede7535611b4e1fdeb35b5ba2b86bc6b761
Slimobedsama <ohwo.okoro@yahoo.com>
2024-05-14 01:02:27 +0100

    db array created

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 const http = require('http');
+const { title } = require('process');
 const PORT = 4000;
+
+const db = 
+[
+    {
+        id: '1',
+        title: 'The Nigerian police is your friend',
+        comedian: 'AY',
+        year: 2008
+    },
+    {
+        id: '2',
+        title: 'The smart student',
+        comedian: 'Basket Mouth',
+        year: 2002
+    },
+    {
+        id: '3',
+        title: 'Caught while trying to outsmart mom',
+        comedian: 'Buchi',
+        year: 2015
+    },
+    {
+        id: '4',
+        title: 'Three drunk friends',
+        comedian: 'Bovi',
+        year: 2010
+    },
+];
 
 const handleRequest = ((req, res)=> {
     res.end(JSON.stringify({
